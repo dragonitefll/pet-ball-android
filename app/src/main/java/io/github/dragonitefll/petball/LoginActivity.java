a3f8947ebee8954bcce021d59c87112a7da8cbc9
Anthony <li.anthony917@gmail.com>
2016-10-16 16:04:09 -0400

    Revert "add the cereal connection"

--- app/src/main/java/io/github/dragonitefll/petball/LoginActivity.java
+++ app/src/main/java/io/github/dragonitefll/petball/LoginActivity.java
@@ -216,12 +216,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@")&& email.contains(".");
+        return email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4||password=="Bob";
+        return password.length() > 4;
     }
 
     /**
--- app/src/main/java/io/github/dragonitefll/petball/MainActivity.java
+++ app/src/main/java/io/github/dragonitefll/petball/MainActivity.java
@@ -1,23 +1,7 @@
 package io.github.dragonitefll.petball;
 
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbManager;
-import android.os.Parcel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
-import java.util.HashMap;
-import java.util.Map;
-import android.content.Intent;
-
-import com.felhr.usbserial.UsbSerialDevice;
-import com.felhr.usbserial.UsbSerialInterface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,69 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-
-         final String ACTION_USB_PERMISSION =
-                "com.android.example.USB_PERMISSION";
-        HashMap usbDevices = manager.getDeviceList();
-        if (!usbDevices.isEmpty()) {
-            boolean keep = true;
-
-
-            for ( Map.Entry entry : usbDevices.entrySet()) {
-                UsbDevice device = (UsbDevice) entry.getValue();
-
-                int deviceVID = device.getVendorId();
-                if (deviceVID == 0x2341)//Arduino Vendor ID
-                {
-                    keep = false;
-                } else {
-                    UsbDeviceConnection connection = null;
-                    device = null;
-                }
-
-                if (!keep)
-                    break;
-            }
-
-
-
-         final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
-                    boolean granted =
-                            intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
-                    if (granted) {
-                        UsbDevice device= UsbDevice.CREATOR.createFromParcel(Parcel.obtain());
-                        UsbDeviceConnection connection = manager.openDevice(device);
-                        UsbSerialDevice serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
-                        if (serialPort != null) {
-                            if (serialPort.open()) { //Set Serial Connection Parameters.serialPort.setBaudRate(9600);
-                                serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
-                                serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
-                                serialPort.setParity(UsbSerialInterface.PARITY_NONE);
-                                serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-                                serialPort.read(mCallback); //
-                                tvAppend(textView, "Serial Connection Opened!\n");
-
-                            } else {
-                                Log.d("SERIAL", "PORT NOT OPEN");
-                            }
-                        } else {
-                            Log.d("SERIAL", "PORT IS NULL");
-                        }
-                    } else {
-                        Log.d("SERIAL", "PERM NOT GRANTED");
-                    }
-                } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
-                    onClickStart(startButton);
-                } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
-                    onClickStop(stopButton);
-                }
-            }
-
-            ;
-        };
-    }}
+    }
 }
